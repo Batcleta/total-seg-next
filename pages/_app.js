c335849f4b03d4f242fedf4367bbe2195c3071fc
Batcleta <thewazig.wf@gmail.com>
2021-09-30 17:43:39 -0300

    GTM

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,24 @@ import "../styles/globals.css";
 import Head from "next/head";
 import { AppWrapper } from "../context/appContext";
 
+import { gtmVirtualPageView } from "../lib/gtm";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const mainDataLayer = {
+      pageTypeName: pageProps.page || null,
+      url: router.pathname,
+    };
+
+    window.onload = () => window.dataLayer?.push({ ...mainDataLayer });
+
+    gtmVirtualPageView(mainDataLayer);
+  }, [pageProps]);
+
   return (
     <>
       <Head>
